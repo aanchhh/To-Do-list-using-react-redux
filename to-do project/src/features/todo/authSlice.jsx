44edc7d6e114a39ba4a12c2365213a409ff11387
aanchal <aanchalmehta.9114@gmail.com>
2024-12-19 11:50:02 +0530

    added comments

--- to-do project/src/features/todo/authSlice.jsx
+++ to-do project/src/features/todo/authSlice.jsx
@@ -1,28 +1,40 @@
+// authSlice: It manages the authentication state, such as whether the user is logged in or not
+// It passes data (authentication status, user info, token) to the store
+// It includes functions to log in and log out the user
+
 import { createSlice } from '@reduxjs/toolkit';
 
+// Initial state that defines the default values before any user input or authentication actions
 const initialState = {
-  isAuthenticated: false,
-  user: null,
-  token: null,
+  isAuthenticated: false,  // User is not authenticated initially
+  user: null,  // User information is null initially
+  token: null,  // No authentication token initially
 };
 
+// Creating the 'auth' slice that contains the state and reducers for authentication actions
 const authSlice = createSlice({
-  name: 'auth',
-  initialState,
+  name: 'auth',  // Name of the slice for identification
+  initialState,  // The initial state defined above
   reducers: {
+    // login reducer: called when the user successfully logs in
     login(state, action) {
-      const { user, token } = action.payload;
-      state.isAuthenticated = true;
-      state.user = user;
-      state.token = token;
+      const { user, token } = action.payload;  // Destructuring user and token from action payload
+      state.isAuthenticated = true;  // Set isAuthenticated to true after login
+      state.user = user;  // Store the user data in state
+      state.token = token;  // Store the authentication token in state
     },
+
+    // logout reducer: called when the user logs out
     logout(state) {
-      state.isAuthenticated = false;
-      state.user = null;
-      state.token = null;
+      state.isAuthenticated = false;  // Set isAuthenticated to false after logout
+      state.user = null;  // Clear user data
+      state.token = null;  // Clear authentication token
     },
   },
 });
 
+// Exporting the individual reducers as actions to be used by the components
 export const { login, logout } = authSlice.actions;
+
+// Exporting the reducer to be included in the store
 export default authSlice.reducer;
--- to-do project/src/features/todo/todoSlice.jsx
+++ to-do project/src/features/todo/todoSlice.jsx
@@ -1,12 +1,15 @@
+//todoSlice: it basically consists of the data/feature/function
+//it passes data to the store.jsx
+//has two main methods - createSlice (allows to create feature), initialState (defines what happens before the user i/p)
 import {createSlice, nanoid } from '@reduxjs/toolkit';
 const initialState = {
-    todos: [{id: 1, text: "Learning React-Redux"}]
+    todos: [{id: 1, text: "Learning React-Redux"}] //deafult-values already displayed when page is loaded, state:0 
 }
-export const todoSlice = createSlice({
-    name: 'todo',
+export const todoSlice = createSlice({ //passed to store
+    name: 'todo', 
     initialState,
-    reducers: {
-        addTodo: (state, action) => {
+    reducers: { //consists of the features/ functions 
+        addTodo: (state, action) => { //every function can have two functionalities - state and action
             const todo = {
                 id: nanoid(), 
                 text: action.payload
@@ -14,9 +17,9 @@ export const todoSlice = createSlice({
             state.todos.push(todo)
         },
         removeTodo: (state, action) => {
-            state.todos = state.todos.filter((todo) => todo.id !== action.payload )
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload ) //payload comes with action: used with dispatch - when some info is passed when an action is performed, payload is used
         },
     }
 })
-export const {addTodo, removeTodo} = todoSlice.actions
-export default todoSlice.reducer
+export const {addTodo, removeTodo} = todoSlice.actions //exported individually because they can be independently used
+export default todoSlice.reducer //used in store
